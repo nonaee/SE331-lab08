dfd007cc518abb46248f2d69ecb0385c7fd11c79
NONAE\goes__000 <ranate_sirinoy@cmu.ac.th>
2017-10-27 16:21:44 +0700

    Q4.6

--- src/main/java/camt/cbsd/lab05/repository/StudentRepository.java
+++ src/main/java/camt/cbsd/lab05/repository/StudentRepository.java
@@ -4,6 +4,7 @@ import camt.cbsd.lab05.entity.Student;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
+    Student findById(long id);
 
 
 }
