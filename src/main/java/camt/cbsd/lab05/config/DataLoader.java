15ae76608c97b5407b23969003faee3d5fe5459a
NONAE\goes__000 <ranate_sirinoy@cmu.ac.th>
2017-10-29 20:39:32 +0700

    Q5.7

--- src/main/java/camt/cbsd/lab05/config/DataLoader.java
+++ src/main/java/camt/cbsd/lab05/config/DataLoader.java
@@ -1,24 +1,30 @@
 package camt.cbsd.lab05.config;
 
+import camt.cbsd.lab05.dao.CourseDao;
 import camt.cbsd.lab05.dao.StudentDao;
+import camt.cbsd.lab05.entity.Course;
 import camt.cbsd.lab05.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @ConfigurationProperties(prefix = "server")
 @Component
 public class DataLoader implements ApplicationRunner {
-    StudentDao studentDao;
+    StudentDao  studentDao ;
+    CourseDao courseDao;
+
+
     @Autowired
     public void setStudentDao(StudentDao studentDao){
-        this.studentDao= studentDao;
+        this.studentDao = studentDao;
     }
-    String baseUrl;
-    String imageUrl;
-    String imageBaseUrl;
+
+    @Autowired
+    public void setCourseDao(CourseDao courseDao) { this.courseDao = courseDao; }
 
     public void setBaseUrl(String baseUrl) {
         this.baseUrl = baseUrl;
@@ -28,14 +34,45 @@ public class DataLoader implements ApplicationRunner {
         this.imageUrl = imageUrl;
     }
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        imageBaseUrl= baseUrl+ imageUrl;
-        studentDao.addStudent(Student.builder().studentId("SE-001").name("Mitsuha").surname("Miyamizu").gpa(2.15).image(imageBaseUrl+"mitsuha.gif").feature(true).penAmount(0).description("The most beloved one").build());
-        studentDao.addStudent(Student.builder().studentId("SE-002").name("Prayuth").surname("The minister").gpa(3.95).image(imageBaseUrl+"tu.gif").feature(false).penAmount(15).description("The great man ever!!!").build());
-        studentDao.addStudent(Student.builder().studentId("SE-003").name("Jurgen").surname("Kloop").gpa(2.15).image(imageBaseUrl+"Kloop.gif").feature(true).penAmount(2).description("The man for the Kop").build());
+    String baseUrl;
+    String imageUrl;
+    String imageBaseUrl;
 
-        Student student = new Student(1,"SE-001","Mitsuha","Miyamizu",2.15,imageBaseUrl+"mitsuha.gif",true,0,"The most beloved one");
+    public void setImageBaseUrl(String imageBaseUrl) {
+        this.imageBaseUrl = imageBaseUrl;
+    }
+
+    @Override
+    @Transactional
+    public void run(ApplicationArguments args) throws Exception {
+        imageBaseUrl = baseUrl + imageUrl;
+
+        Student student1 = Student.builder().studentId("SE-001").name("Mitsuha").surname("Miyamizu")
+                .gpa(2.15).image(imageBaseUrl+"mitsuha.jpg").feature(true).penAmount(0).description("The most beloved one").build();
+        Student student2 = Student.builder().studentId("SE-002").name("Prayuth").surname("The minister")
+                .gpa(3.59).image(imageBaseUrl+"tu.gif").feature(false).penAmount(15).description("The great man ever!!!!").build();
+        Student student3 = Student.builder().studentId("SE-003").name("Jurgen").surname("Kloop")
+                .gpa(2.15).image(imageBaseUrl+"Kloop.gif").feature(true).penAmount(2).description("The man for Kop").build();
+
+        Course course1 = Course.builder().courseId("953331").courseName("CBSD").build();
+        Course course2 = Course.builder().courseId("953323").courseName("Software Construction").build();
+        Course course3 = Course.builder().courseId("953499").courseName("Software Project").build();
+
+
+        courseDao.add(course1);
+        courseDao.add(course2);
+        courseDao.add(course3);
+        studentDao.addStudent(student1);
+        studentDao.addStudent(student2);
+        studentDao.addStudent(student3);
+
+        student1.addCourse(course1);
+        student1.addCourse(course2);
+        student2.addCourse(course2);
+        student2.addCourse(course3);
+        student3.addCourse(course1);
+        student3.addCourse(course3);
+
 
 
     }
--- src/main/java/camt/cbsd/lab05/entity/Student.java
+++ src/main/java/camt/cbsd/lab05/entity/Student.java
@@ -6,32 +6,41 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 
 @Entity
 @Data
-@NoArgsConstructor
-
 @AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 public class Student {
+    @ManyToMany
+    List<Course> enrolledCourse;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 
-    long id;
-    String studentId;
-    String name;
-    String surname;
-    double gpa;
-    String image;
-    boolean feature;
-    int penAmount;
-    String description;
+    @NonNull long id;
+    @NonNull String studentId;
+    @NonNull String name;
+    @NonNull String surname;
+    @NonNull double gpa;
+    @NonNull String image;
+    @NonNull boolean feature;
+    @NonNull int penAmount;
+    @NonNull String description;
+
+    public List<Course> addCourse(Course course){
+        enrolledCourse = Optional.ofNullable(enrolledCourse).orElse(new ArrayList<>());
+        enrolledCourse.add(course);
+        return enrolledCourse;
+    }
 
 
 //    public String getName() {
